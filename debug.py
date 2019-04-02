4a214296834d352678cc7ab1e69cbe30e535d681
ixhorse <cyfhorse@gmail.com>
2019-04-02 10:21:42 +0800

    add random crop(gluoncv)

--- debug.py
+++ debug.py
@@ -3,11 +3,10 @@ import torch
 from utils.dataset_voc import VOCDetection
 import pdb
 
-voc = VOCDetection(root=os.path.join('~', 'data', 'VOCdevkit'), batch_size=1,
-                   img_size=416)
+voc = VOCDetection(root=os.path.join('~', 'data', 'VOCdevkit'), img_size=416)
 
-dataloader = torch.utils.data.DataLoader(voc, batch_size=16,
-    num_workers=8, pin_memory=False)
+dataloader = torch.utils.data.DataLoader(voc, batch_size=1,
+    num_workers=1, pin_memory=False)
 
-for imgs, targets, numboxes, _ in dataloader:
-    print(sum(numboxes))
+for imgs, targets, shapes, _ in dataloader:
+    print(targets)
